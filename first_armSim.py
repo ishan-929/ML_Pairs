a1d190be12eadddd648fdf12ca29af5372d183e2
Ishan Karmakar <karmakarbablu067@gmail.com>
2026-09-01 00:01:10 -0400

    Seperating Arm and Object Mass as well as adding Motor Torque

--- first_armSim.py
+++ first_armSim.py
@@ -6,16 +6,21 @@ angular_velocity = 0.0
 dt = 0.01
 
 gravity = 9.81
-mass = 1
+arm_mass = 0.5
+payload_mass = 1.0
 arm_length = 0.5
 
-motor_torque = 0.0
+motor_torque = 6.0
 
 damping = 0.2
 
 for step in range (300):
-    gravity_torque = -mass * gravity * arm_length * math.cos(angle)
-    inertia = mass * arm_length ** 2
+    arm_gravity_torque = -arm_mass * gravity * (arm_length/2) * math.cos(angle)
+    payload_gravity_torque = -payload_mass * gravity * arm_length * math.cos(angle)
+    gravity_torque = payload_gravity_torque + arm_gravity_torque
+    arm_inertia = (1/3) * arm_mass * arm_length **2
+    payload_inertia = payload_mass * arm_length ** 2
+    inertia = arm_inertia + payload_inertia
     damping_torque = -damping * angular_velocity
     net_torque = motor_torque + gravity_torque + damping_torque
 
